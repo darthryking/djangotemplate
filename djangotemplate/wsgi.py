bb180377ae6dfccfa36986c7bd9d1bb67c4efabd
Ryan Lam <darth.ryking@gmail.com>
2014-06-24 19:33:39 -0400

    hopefully fixed static files

--- djangotemplate/wsgi.py
+++ djangotemplate/wsgi.py
@@ -30,3 +30,9 @@ application = get_wsgi_application()
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
 # application = HelloWorldApplication(application)
+
+try:    # It's okay if this Cling thingy doesn't exist, because if it doesn't exist, it's not running on Heroku.
+    from dj_static import Cling     # (try... except added by yamcat)
+    application = Cling(get_wsgi_application())
+except ImportError:
+    pass
