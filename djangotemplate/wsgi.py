6635ad2b51f792b47df8b53dfe79c1422eda3290
Ryan Lam <darth.ryking@gmail.com>
2014-09-04 22:56:42 -0400

    put wsgi.py edits back

--- djangotemplate/wsgi.py
+++ djangotemplate/wsgi.py
@@ -12,3 +12,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangotemplate.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+try:    # It's okay if this Cling thingy doesn't exist, because if it doesn't exist, it's not running on Heroku.
+    from dj_static import Cling     # (try... except added by yamcat)
+    application = Cling(get_wsgi_application())
+except ImportError:
+    pass
+    
